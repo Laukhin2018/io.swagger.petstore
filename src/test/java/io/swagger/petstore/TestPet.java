95570dfd0a81e7c152a5f4bd186b400481c2eb23
Ilya <i.laukhin@hybrid.ru>
2020-01-23 17:10:45 +0300

    Получилось отправить запрос

--- src/test/java/io/swagger/petstore/TestPet.java
+++ src/test/java/io/swagger/petstore/TestPet.java
@@ -4,16 +4,26 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestPet {
 
+
     @Test
-    public void petTest(){
-        RestAssured.given()
-                .baseUri("https://www.wiley.com")
+    public void part1Test1(){
+        given().contentType(ContentType.JSON).when().get("https://www.wiley.com/en-us/search/autocomplete/comp_00001H9J?term=Java")
+                .then()
+                .extract().response().prettyPrint();
+    }
+
+    @Test
+    public void part1Test2(){
+        given().baseUri("https://www.wiley.com")
                 .basePath("/en-us/search/autocomplete/comp_00001H9J")
                 .contentType(ContentType.JSON)
-                .header("term", "Java")
-                .when().get()
+                .when()
+                .queryParam("term" , "Java")
+                .get()
                 .then()
                 .extract().response().prettyPrint();
     }
